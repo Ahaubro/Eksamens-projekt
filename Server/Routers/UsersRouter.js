1889dbdb42a0a77d3812a00ec1c70fbd0d205ee7
Malte <fe12malhar@gmail.com>
2022-05-18 14:34:11 +0200

    playing around with the edit user method, trying to get it to fully work with single and mulitple changes on the profile

--- Server/Routers/UsersRouter.js
+++ Server/Routers/UsersRouter.js
@@ -95,11 +95,11 @@ router.post("/auth/signup", async (req, res) => {
 
         if (result[0]) {
             resUsername = result[0].username
-            resMail = result[0].email 
-            
+            resMail = result[0].email
+
             return res.status(404).send("There is already a user with the username or email ")
         } else {
-            
+
             async function hash() {
                 const hashedPassword = await bcrypt.hash(password, saltRounds);
 
@@ -132,16 +132,17 @@ router.get("/auth/getProfile", async (req, res) => {
             res.send(err)
 
         if (result[0]) {
-            username = result[0].username
-            firstname = result[0].firstname
-            middlename = result[0].middlename
-            lastname = result[0].lastname
-            birthday = result[0].birthday
-            address = result[0].address
-            country = result[0].country
-            city = result[0].city
-            zipcode = result[0].zipcode
-            profilecolor = result[0].profilecolor
+            let { username: username, password: password, firstname: firstname, middlename: middlename, lastname: lastname, birthday: birthday, address: address, country: country, city: city, zipcode: zipcode, profilecolor: profilecolor } = result[0]
+            // username = result[0].username
+            // firstname = result[0].firstname
+            // middlename = result[0].middlename
+            // lastname = result[0].lastname
+            // birthday = result[0].birthday
+            // address = result[0].address
+            // country = result[0].country
+            // city = result[0].city
+            // zipcode = result[0].zipcode
+            // profilecolor = result[0].profilecolor
             res.send(JSON.stringify({ username, firstname, middlename, lastname, birthday, address, country, city, zipcode, profilecolor }))
         } else {
             res.send("didnt find anything")
@@ -153,7 +154,7 @@ router.patch("/auth/editProfile", async (req, res) => {
 
     const id = req.session.userID
     const { username, password, firstname, middlename,
-lastname, birthday, address, country, city, zipcode, profilecolor } = req.body
+    lastname, birthday, address, country, city, zipcode, profilecolor } = req.body
 
     let sqlSelect = "SELECT * FROM users WHERE id = ?";
     const foundUser = await db.query(sqlSelect, [id], function (err, result) {
@@ -168,13 +169,19 @@ lastname, birthday, address, country, city, zipcode, profilecolor } = req.body
         } else {
             //MANGLER BCRYPT PÅ PASSWORD CHANGE
             async function editProfile() {
-                let updateProfile1 = "UPDATE `smiles`.`users` SET username = ?, password = ?, firstname = ?, middlename = ?, lastname = ?, birthday = ?, address = ?, country = ?, city = ?, zipcode = ?, profilecolor = ? WHERE id = ?"
-                const editQuery = await db.query(updateProfile1, [username, password, firstname, middlename, lastname, birthday, address, country, city, zipcode, profilecolor, id], function (err, result) {
+                const hashedPassword = await bcrypt.hash(password, saltRounds);
+                console.log(birthday, typeof(birthday))
+                let updateProfile1 = `UPDATE users SET username = IFNULL(?, users.username), password = IFNULL(?, password), firstname = IFNULL(?, firstname), 
+                    middlename = IFNULL(?, middlename), lastname = IFNULL(?, lastname), birthday = IFNULL(?, "2022-01-01"), address = IFNULL(?, address), 
+                    country = IFNULL(?, country), city = IFNULL(?, city), zipcode = IFNULL(?, zipcode), profilecolor = IFNULL(?, profilecolor) WHERE id = ?`
+                const editQuery = await db.query(updateProfile1, [username, hashedPassword, firstname, middlename, lastname, birthday, address, country, city, zipcode, profilecolor, id], function (err, result) {
+                    console.log("kommer du herind?")
 
                     if (err) {
+                        console.log(err)
                         return res.send("something went wrong")
                     }
-                    
+                    console.log("hvad så, kommer du herind?")
                     return res.send("Your profile have now been changed")
                 });
             }
@@ -182,7 +189,202 @@ lastname, birthday, address, country, city, zipcode, profilecolor } = req.body
         }
 
     });
-})
+
+    // const id = req.session.userID;
+    
+    // const { username, password, firstname, middlename,
+    //     lastname, birthday, address, country, city, zipcode, profilecolor } = req.body
+
+    // let sqlSelect = "SELECT * FROM users WHERE id = ?";
+    // const foundUser = await db.query(sqlSelect, [id], function (err, result) {
+        
+    //     const { username: DBusername, password: DBpassword, firstname: DBfirstname, middlename: DBmiddlename, 
+    //         lastname: DBlastname, birthday: DBbirthday, address: DBaddress, country: DBcountry, city: DBcity, 
+    //         zipcode: DBzipcode, profilecolor: DBprofilecolor } = result[0]
+        
+    //     if (err) {
+    //         return res.status(400).send();
+    //     }
+
+    //     if (!id) {
+    //         return res.send("Something went wrong - contact an admin");
+
+    //     } else {
+    //         async function editProfile(id, user){
+    //             const result = await db.query(
+    //               `UPDATE users
+    //               SET username="${user.username}", password=${user.password}, firstname=${user.firstname}, 
+    //               middlename=${user.middlename}, lastname=${user.lastname}, birthday=${user.birthday}, 
+    //               address=${user.address}, country=${user.country}, city=${user.city}, zipcode=${user.zipcode},
+    //               profilecolor=${user.profilecolor} WHERE id=${id}` 
+    //             );
+              
+    //             let message = 'Error in updating programming language';
+              
+    //             if (result.affectedRows) {
+    //               message = 'Programming language updated successfully';
+    //             }
+              
+    //             return {message};
+    //           }
+        
+    //         console.log("1kommer den ind her?")
+    //         MANGLER BCRYPT PÅ PASSWORD CHANGE
+    //         async function editProfile() {
+                
+    //             if( username !== undefined ){
+    //                 const hashedPassword = await bcrypt.hash(password, saltRounds);
+    //                 const editUsername = await db.query(updateProfile, [username, hashedPassword, firstname, middlename, 
+    //                     lastname, birthday, address, country, city, zipcode, profilecolor, id], function (err, result) {
+    //                     //bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             }
+    //             console.log(username)
+    //             if( username !== undefined ){
+    //                 const updateUsername = "UPDATE `smiles`.`users` SET username = ? WHERE id = ?"
+    //                 console.log("2kommer den ind her?")
+    //                 const editUsername = await db.query(updateUsername, [username, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             }
+    //             if (password !== undefined) {
+    //                 const updatePassword = "UPDATE `smiles`.`users` SET password = ? WHERE id = ?"
+
+    //                 const hashedPassword = await bcrypt.hash(password, saltRounds);
+    //                 const editPassword = await db.query(updatePassword, [hashedPassword, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             } 
+    //             if (firstname !== undefined) {
+    //                 const updateFirstname = "UPDATE `smiles`.`users` SET firstname = ? WHERE id = ?"
+
+    //                 const editFirstname = await db.query(updateFirstname, [firstname, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             } 
+    //             if (middlename !== undefined) {
+    //                 const updateMiddlename = "UPDATE `smiles`.`users` SET middlename = ? WHERE id = ?"
+
+    //                 const editMiddlename = await db.query(updateMiddlename, [middlename, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             } 
+    //             if (lastname !== undefined) {
+    //                 const updateLastname = "UPDATE `smiles`.`users` SET lastname = ? WHERE id = ?"
+
+    //                 const editLastname = await db.query(updateLastname, [lastname, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             } 
+    //             if (birthday !== undefined) {
+    //                 const updateBirthday = "UPDATE `smiles`.`users` SET birthday = ? WHERE id = ?"
+
+    //                 const editBirthDay = await db.query(updateBirthday, [birthday, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             } 
+    //             if (address !== undefined) {
+    //                 const updateAddress = "UPDATE `smiles`.`users` SET address = ? WHERE id = ?"
+
+    //                 const editAddress = await db.query(updateAddress, [address, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             } 
+    //             if (country !== undefined) {
+    //                 const updateCountry = "UPDATE `smiles`.`users` SET country = ? WHERE id = ?"
+
+    //                 const editCountry = await db.query(updateCountry, [city, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             } 
+    //             if (city !== undefined) {
+    //                 const updateCity = "UPDATE `smiles`.`users` SET city = ? WHERE id = ?"
+             
+    //                 const editZipCode = await db.query(updateCity, [zipcode, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             } 
+    //             if (zipcode !== undefined) {
+    //                 const updateZipcode = "UPDATE `smiles`.`users` SET zipcode = ? WHERE id = ?"
+
+             
+    //                 const editZipCode = await db.query(updateZipcode, [zipcode, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             } 
+    //             if (profilecolor !== undefined) {
+    //                 const updateProfilecolor = "UPDATE `smiles`.`users` SET profilecolor = ? WHERE id = ?"
+
+    //                 const editProfileColor = await db.query(updateProfilecolor, [profilecolor, id], function (err, result) {
+    //                     bcrypt
+    //                     if (err) {
+    //                         return res.send("something went wrong")
+    //                     }
+
+    //                     return res.send("Your profile have now been changed")
+    //                 });
+    //             }
+
+    //         editProfile();
+    //             }
+    //             update();
+    //         });    
+    });
 
 // Send mail using nodemailer function
 function sendMail(email) {
