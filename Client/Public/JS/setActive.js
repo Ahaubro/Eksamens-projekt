ea46834da8042b2c25f2883e09b4ac9049eb9915
Malte <fe12malhar@gmail.com>
2022-06-24 17:41:57 +0200

    checked for more errors. Now it is specified which sites that can be active

--- Client/Public/JS/setActive.js
+++ Client/Public/JS/setActive.js
@@ -1,18 +1,20 @@
 function setActive() {
 
     let currentPage = window.location.pathname.substring(1)
+
+
+    
+
     if (currentPage === "") currentPage = "home"
     let aTags = document.getElementsByTagName('a')
-    console.log(currentPage)
-    console.log(aTags.length)
-    for (let i = 0; i < aTags.length; i++) {
-        console.log("each atag", aTags[i])
-        if (aTags[i].className === "active") {
-            console.log("atag to edit", aTags[i])
-            aTags[i].className = ""
-        }
 
-        document.getElementById(currentPage).className = "active"
+    if (currentPage.includes("home") || currentPage.includes("smileposts") || currentPage.includes("chatrooms") || currentPage.includes("mypage")) {
+        for (let i = 0; i < aTags.length; i++) {
+            if (aTags[i].className === "active") {
+                aTags[i].className = ""
+            }
+            document.getElementById(currentPage).className = "active"
+        }
     }
 }
 setActive();
