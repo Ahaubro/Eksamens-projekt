65cde0218f896844f0f50595114f87caca6f0ea3
Ahaubro <alex_haubro@hotmail.com>
2022-06-25 11:01:13 +0200

    Clean

--- Client/Public/JS/setActive.js
+++ Client/Public/JS/setActive.js
@@ -3,12 +3,8 @@ function setActive() {
     let currentPage = window.location.pathname.substring(1)
     if (currentPage === "") currentPage = "home"
     let aTags = document.getElementsByTagName('a')
-    console.log(currentPage)
-    console.log(aTags.length)
     for (let i = 0; i < aTags.length; i++) {
-        console.log("each atag", aTags[i])
         if (aTags[i].className === "active") {
-            console.log("atag to edit", aTags[i])
             aTags[i].className = ""
         }
 
